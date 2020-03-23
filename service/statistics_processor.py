ebf21ea7640ec3f6720f1a8defa948962ab59851
lerz30 <lereyeszabala@gmail.com>
2020-03-23 10:27:36 +0100

    First team statistics

--- service/statistics_processor.py
+++ service/statistics_processor.py
@@ -1,6 +1,5 @@
 from pyspark.sql import *
 
-
 ss = SparkSession \
     .builder \
     .appName("English Premier League match statistics") \
@@ -46,10 +45,112 @@ statistics = raw_statistics_df\
     .withColumnRenamed("HR", "home_red")\
     .withColumnRenamed("AR", "away_red")
 statistics.printSchema()
+statistics.limit(5).show()
+
+'''
+TEAMS STATISTICS
+'''
+#
+#Games played
+#
+home_matches = statistics\
+    .groupBy("HomeTeam")\
+    .count()\
+    .withColumnRenamed("HomeTeam", "Team")\
+    .withColumnRenamed("count", "Home_matches")
+
+away_matches = statistics\
+    .groupBy("AwayTeam")\
+    .count()\
+    .withColumnRenamed("AwayTeam", "Team")\
+    .withColumnRenamed("count", "Away_matches")
+
+total_matches = home_matches\
+    .join(away_matches, "Team", "inner")\
+    .withColumn("matches_played", home_matches.Home_matches + away_matches.Away_matches)\
+    .drop("Home_matches")\
+    .drop("Away_matches")
+total_matches.printSchema()
+
+#
+#Top 5 Wins
+#
+home_wins = statistics\
+    .where(statistics.FT_result == "H")\
+    .groupBy("HomeTeam")\
+    .count()\
+    .withColumnRenamed("count", "home_wins")\
+    .withColumnRenamed("HomeTeam", "Team")
+home_wins.printSchema()
+
+away_wins = statistics\
+    .where(statistics.FT_result == "A")\
+    .groupBy("AwayTeam")\
+    .count()\
+    .withColumnRenamed("count", "away_wins")\
+    .withColumnRenamed("AwayTeam", "Team")
+away_wins.printSchema()
+
+total_wins = home_wins\
+    .join(away_wins, "Team", "inner")\
+    .withColumn("total_wins", home_wins.home_wins + away_wins.away_wins)
+total_wins.printSchema()
+
+total_wins = total_wins\
+    .join(total_matches, "Team", "inner")\
+    .withColumn("Win %", (total_wins.total*100)/total_matches.matches_played)\
+    .orderBy("total_wins", ascending=False)\
+    .show()
+
+#
+#Most scored goals
+#
+home_team_goals = statistics\
+    .select("HomeTeam", "FT_homeGoals")\
+    .groupBy("HomeTeam")\
+    .sum("FT_homeGoals")\
+    .withColumnRenamed("HomeTeam", "Team")\
+    .withColumnRenamed("sum(FT_homeGoals)", "home_goals")\
+    .orderBy("home_goals", ascending = False)
+home_team_goals.printSchema()
+
+away_team_goals = statistics\
+    .select("AwayTeam", "FT_awayGoals")\
+    .groupBy("AwayTeam")\
+    .sum("FT_awayGoals")\
+    .withColumnRenamed("AwayTeam", "Team")\
+    .withColumnRenamed("sum(FT_awayGoals)", "away_goals")\
+    .orderBy("away_goals", ascending = False)
+away_team_goals.printSchema()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 '''
 Referees statistics and analysis
-'''
+
+
 referees = statistics\
     .select("Referee")\
     .distinct()\
@@ -71,7 +172,5 @@ least_referred_games = statistics\
     .orderBy("matches")\
     .limit(10)\
     .show()
+'''
 
-'''
-Teams statistics
-'''
