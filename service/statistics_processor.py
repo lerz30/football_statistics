e03ebda08cf0aabe6eef5378ccf7dc0ac5ca2524
lerz30 <lereyeszabala@gmail.com>
2020-03-24 16:28:16 +0100

    First team statistics

--- service/statistics_processor.py
+++ service/statistics_processor.py
@@ -50,9 +50,7 @@ statistics.limit(5).show()
 '''
 TEAMS STATISTICS
 '''
-#
-#Games played
-#
+
 home_matches = statistics\
     .groupBy("HomeTeam")\
     .count()\
@@ -73,7 +71,7 @@ total_matches = home_matches\
 total_matches.printSchema()
 
 #
-#Top 5 Wins
+# Wins statistics
 #
 home_wins = statistics\
     .where(statistics.FT_result == "H")\
@@ -91,37 +89,80 @@ away_wins = statistics\
     .withColumnRenamed("AwayTeam", "Team")
 away_wins.printSchema()
 
-total_wins = home_wins\
+wins_stats = home_wins\
     .join(away_wins, "Team", "inner")\
     .withColumn("total_wins", home_wins.home_wins + away_wins.away_wins)
-total_wins.printSchema()
+wins_stats.printSchema()
 
-total_wins = total_wins\
-    .join(total_matches, "Team", "inner")\
-    .withColumn("Win %", (total_wins.total*100)/total_matches.matches_played)\
+wins_stats = total_matches\
+    .join(wins_stats, "Team", "inner")\
+    .withColumn("Win %", (wins_stats.total_wins*100)/total_matches.matches_played)
+wins_stats.printSchema()
+
+#
+# Draws statistics
+#
+home_draws = statistics\
+    .where(statistics.FT_result == "D")\
+    .groupBy("HomeTeam")\
+    .count()\
+    .withColumnRenamed("count", "home_draws")\
+    .withColumnRenamed("HomeTeam", "Team")
+home_draws.printSchema()
+
+away_draws = statistics\
+    .where(statistics.FT_result == "D")\
+    .groupBy("AwayTeam")\
+    .count()\
+    .withColumnRenamed("count", "away_draws")\
+    .withColumnRenamed("AwayTeam", "Team")
+away_draws.printSchema()
+
+draw_stats = home_draws\
+    .join(away_draws, "Team", "inner")\
+    .withColumn("total_draws", home_draws.home_draws + away_draws.away_draws)
+draw_stats.printSchema()
+
+#Output table
+games_table = wins_stats\
+    .join(draw_stats, "Team", "inner")\
+    .withColumn("Draw %", (draw_stats.total_draws*100)/wins_stats.matches_played)\
     .orderBy("total_wins", ascending=False)\
     .show()
 
+
+
 #
-#Most scored goals
+# Goals statistics
 #
 home_team_goals = statistics\
-    .select("HomeTeam", "FT_homeGoals")\
+    .select("HomeTeam", "FT_homeGoals", "FT_awayGoals")\
     .groupBy("HomeTeam")\
-    .sum("FT_homeGoals")\
+    .sum("FT_homeGoals", "FT_awayGoals")\
     .withColumnRenamed("HomeTeam", "Team")\
-    .withColumnRenamed("sum(FT_homeGoals)", "home_goals")\
-    .orderBy("home_goals", ascending = False)
+    .withColumnRenamed("sum(FT_homeGoals)", "home_scored")\
+    .withColumnRenamed("sum(FT_awayGoals)", "home_received")
 home_team_goals.printSchema()
 
 away_team_goals = statistics\
-    .select("AwayTeam", "FT_awayGoals")\
+    .select("AwayTeam", "FT_homeGoals", "FT_awayGoals")\
     .groupBy("AwayTeam")\
-    .sum("FT_awayGoals")\
+    .sum("FT_homeGoals", "FT_awayGoals")\
     .withColumnRenamed("AwayTeam", "Team")\
-    .withColumnRenamed("sum(FT_awayGoals)", "away_goals")\
-    .orderBy("away_goals", ascending = False)
+    .withColumnRenamed("sum(FT_homeGoals)", "away_received")\
+    .withColumnRenamed("sum(FT_awayGoals)", "away_scored")
 away_team_goals.printSchema()
+
+goals_stats = home_team_goals\
+    .join(away_team_goals, "Team", "inner")\
+    .withColumn("total_scored", home_team_goals.home_scored + away_team_goals.away_scored)\
+    .withColumn("total_received", home_team_goals.home_received + away_team_goals.away_received)
+goals_stats.printSchema()
+
+goals_stats = goals_stats\
+    .withColumn("goal_dif", goals_stats.total_scored - goals_stats.total_received)\
+    .orderBy("goal_dif", ascending = False)\
+    .show()
 
 
 
